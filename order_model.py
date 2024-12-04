e7363eef01123b4a1b4f48ae03297d4306a2aff0
K. Fierens <100869833+fierensk@users.noreply.github.com>
2024-12-04 17:50:38 -0500

    saving to db

---
+++ order_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy.orm import declarative_base
+import datetime as dt
+import os
+
+Base = declarative_base()
+
+class Order(Base):
+
+    __tablename__ = "Orders"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    customer_name = Column("customer_name", String, nullable=False)
+    orders = Column("orders", String, nullable=False)
+    order_number = Column("table_number", Integer, nullable=False)
+    date_created = Column("date_created", Date, nullable=False, default=dt.datetime.now(dt.timezone.utc))
--- restaurant_app.py
+++ restaurant_app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, url_for, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from order_model import Base, Order
 
 app = Flask(__name__)
+
+engine = create_engine("sqlite:///orders.db", echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/<int:tableNum>")
 def main_menu(tableNum):
@@ -13,12 +21,21 @@ def order(table_number):
         item.replace("\r", "").strip() for item in order_items_in_String_format.split("\n") if item.strip()
     ]
     customer_name = request.form["customer_name"]
+    # creating an Order object to be saved into the database
+    new_order = Order(customer_name=customer_name, 
+                      orders=order_items_in_String_format, 
+                      order_number=table_number)
     current_order = { 
         "customer" : customer_name, 
         "order_items" : order_items_in_array_format, 
-        "table_number" : table_number}
-
-    return render_template("order_summary.html", order = current_order)
+        "order_number" : table_number}
+    
+    try:
+        session.add(new_order)
+        session.commit()
+        return render_template("order_summary.html", order=current_order)
+    except:
+        return 'There was an issue adding your task'
 
 if __name__ == "__main__":
     app.run(debug=True)
